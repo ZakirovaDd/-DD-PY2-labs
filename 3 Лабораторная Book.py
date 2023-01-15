7fc75c8eb2a65455826b90da8a3cc6134691c100
ZakirovaDd <115179380+ZakirovaDd@users.noreply.github.com>
2023-01-15 19:02:39 +0300

    Лабораторная 2 исправленная

---
+++ 3 Лабораторная Book.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel, conint
+
+BOOKS_DATABASE = [
+    {
+        "id": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id": 2,
+        "name": "test_name_2",
+        "pages": 400,
+    }
+]
+
+class Book(BaseModel):
+    id_: conint(gt=0)
+    name: str
+    pages: conint(gt = 0)
+
+    def __str__(self) -> str:
+        return f'Книга "{self.name}"'
+
+
+    def __repr__(self) -> str:
+        return f'Book(id_={self.id_!r}, name={self.name!r}, pages={self.pages!r})'
+
+
+if __name__ == '__main__':
+    # инициализируем список книг
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+    ]
+    for book in list_books:
+        print(book)
+
+    print(list_books)
